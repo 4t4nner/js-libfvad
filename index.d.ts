38dc413315a3eb899925b0d09f63e7bc5e91cce4
4t4nner <tannerman@inbox.ru>
2020-02-11 09:44:31 +0700

    fix d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
-declare module "@4t4nner/silence-detector" {
-    export function detectSilence(fileName: string): {
-        silence: boolean,
-        error: string,
-        outFile: string,
-    }
+export function detectSilence(fileName: string): {
+    silence: boolean,
+    error: string,
+    outFile: string,
 }
